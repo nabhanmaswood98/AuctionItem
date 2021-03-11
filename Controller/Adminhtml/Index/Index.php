98afa951191096cff54b0b0af2731ac585d1f09f
nabhanmaswood98 <nabhanmaswood@rightpoint.com>
2021-03-11 16:48:36 -0500

    Add base files for admin custom route

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SomethingDigital\AuctionItem\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Custom Controller for Auction Item Grid
+ *
+ * Class Index
+ * @package SomethingDigital\ContactForm\Controller\Adminhtml\Index
+ */
+class Index implements HttpGetActionInterface
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $pageFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        PageFactory $pageFactory
+    ) {
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        $resultPage = $this->pageFactory->create();
+        return $resultPage;
+    }
+}
